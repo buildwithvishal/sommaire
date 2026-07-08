b257d31ae36fb921e0f6e166273bfc80e7a90ca9
buildwithvishal <vishallakshya2004@gmail.com>
2026-07-09 01:36:06 +0530

    Initial Sommaire setup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+  title: "Sommaire - AI-powered PDF Summarization",
+  description: 'Save hours of reading time, Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.'
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("antialiased", "font-sans", geist.variable)}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import tailwindcss from "eslint-plugin-tailwindcss";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+
+  {
+    plugins: {
+      tailwindcss,
+    },
+    rules: {
+      ...tailwindcss.configs.recommended.rules,
+    },
+  },
+
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
