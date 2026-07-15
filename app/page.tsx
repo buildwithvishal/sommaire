cad2d7273f87c27668f279afb9e6c6fc6279628b
buildwithvishal <vishallakshya2004@gmail.com>
2026-07-16 03:28:46 +0530

    hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/common/bg-gradient";
+import HeroSection from "@/components/home/hero-section";
+import DemoSection from "@/components/home/demo-section";
 
 export default function Home() {
   return (
-    <div className="">
-      {/* <HeroSection /> */}
+    <div className="relative w-full">
+      <BgGradient/>
+      <div className="flex flex-col">
+      <HeroSection />
+      <DemoSection />
+      </div>
+
+      {/* 
+      <HowItWorksSection />
+      <PricingSection />
+      <CTASection /> */}
+
     </div>
   );
 }
